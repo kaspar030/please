aa777164bb6fa62582ec73748ccb6a267ef1bc86
Kaspar Schleiser <kaspar@schleiser.de>
2023-05-21 15:44:21 +0200

    add complete subcommand

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,18 @@
 use clap::{crate_authors, crate_description, crate_version, Arg, Command};
 
+fn task() -> Arg {
+    Arg::new("task")
+        .trailing_var_arg(true)
+        .allow_hyphen_values(true)
+        .num_args(1..100)
+}
+
 pub fn clap() -> clap::Command {
     Command::new(clap::crate_name!())
         .version(crate_version!())
         .author(crate_authors!("\n"))
         .about(crate_description!())
+        .subcommand_negates_reqs(true)
         .arg(
             Arg::new("apikey")
                 .short('k')
@@ -16,11 +24,6 @@ pub fn clap() -> clap::Command {
                 .value_parser(clap::value_parser!(String))
                 .num_args(1),
         )
-        .arg(
-            Arg::new("task")
-                .trailing_var_arg(true)
-                .allow_hyphen_values(true)
-                .num_args(1..100)
-                .required(true),
-        )
+        .arg(task().required(true))
+        .subcommand(Command::new("complete").arg(task()))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,71 @@
+use clap::ArgMatches;
 use openai::chat::{ChatCompletion, ChatCompletionMessage, ChatCompletionMessageRole};
 
 mod cli;
 
 #[tokio::main]
 async fn main() {
-    let m = cli::clap().get_matches();
+    let matches = cli::clap().get_matches();
 
-    openai::set_key(m.get_one::<String>("apikey").unwrap().clone());
+    openai::set_key(matches.get_one::<String>("apikey").unwrap().clone());
 
-    let task: Vec<String> = m
+    match matches.subcommand() {
+        Some(("complete", matches)) => handle_complete(matches).await,
+        Some((&_, matches)) => handle_task(matches).await,
+        None => handle_task(&matches).await,
+    }
+}
+
+async fn handle_task(matches: &ArgMatches) {
+    let task: Vec<String> = matches
         .get_many("task")
         .map_or_else(std::vec::Vec::new, |v| v.cloned().collect());
 
     let task = task.join(" ");
+    let prompt = "You are an assistant returning Linux shell commands that accomplish the following task. Don't add explanations or notes.";
 
+    let result = openai_request(prompt, &task).await;
+
+    println!("{}", result);
+}
+
+async fn handle_complete(matches: &ArgMatches) {
+    use std::io::Read;
+
+    let task: Vec<String> = matches
+        .get_many("task")
+        .map_or_else(std::vec::Vec::new, |v| v.cloned().collect());
+
+    let mut task = task.join(" ");
+
+    if task.is_empty() {
+        task.push_str("Please fix this.");
+    }
+
+    // read from stdin
+    let mut buffer = String::new();
+
+    // stdin() returns a handle to the standard input for your process
+    // and read_to_string() reads from it until EOF or an error occurs.
+    std::io::stdin().read_to_string(&mut buffer).unwrap();
+
+    let prompt = format!("{task} Don't add explanations or notes.");
+
+    let result = openai_request(&prompt, &buffer).await;
+
+    println!("{}", result);
+}
+
+async fn openai_request(prompt: &str, task: &str) -> String {
     let mut messages = vec![ChatCompletionMessage {
         role: ChatCompletionMessageRole::System,
-        content:
-            "You are an assistant returning Linux shell commands that accomplish the following task. Don't add explanations or notes."
-                .to_string(),
+        content: prompt.to_string(),
         name: None,
     }];
 
     messages.push(ChatCompletionMessage {
         role: ChatCompletionMessageRole::User,
-        content: task,
+        content: task.to_string(),
         name: None,
     });
 
@@ -34,7 +75,13 @@ async fn main() {
         .unwrap()
         .unwrap();
 
-    let returned_message = chat_completion.choices.first().unwrap().message.clone();
-
-    println!("{}", &returned_message.content.trim());
+    chat_completion
+        .choices
+        .first()
+        .unwrap()
+        .message
+        .clone()
+        .content
+        .trim()
+        .to_string()
 }
